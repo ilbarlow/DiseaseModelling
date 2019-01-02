1df27013ac4853ee04960f24898609462426a45a
Ida <ilbarlow1@gmail.com>
2019-01-02 17:10:49 +0000

    fastchopChop edit

--- fastChopChop.py
+++ fastChopChop.py
@@ -7,7 +7,7 @@ import pandas as pd
 import sys
 import os
 
-def fastChopChop(input_coords):
+def fastChopChop(input_coords, extra_inputs):
 	"""Input:
 	input_coords - .csv generated from sgDesignInput.py, which has a columns
 	name, chrom, start, end 
@@ -17,6 +17,10 @@ def fastChopChop(input_coords):
 
 	inputDF = pd.read_csv(input_coords)
 
+	outputDir = os.path.join(os.path.dirname(input_coords), 'ChopChop_output')
+	if not os.path.exists(outputDir):
+		os.makedirs(outputDir)
+
 	for i,r in inputDF.iterrows():
 		if r.start>r.end:
 			end = str(r.start)
@@ -25,11 +29,15 @@ def fastChopChop(input_coords):
 			start = str(r.start)
 			end = str(r.end)
 
-		cmd = './chopchop.py -G ce11 -P -o ' + os.path.join(os.path.dirname(input_coords),r['name']) 
-		cmd+= ' ' + r.chrom + ':' + start + '-' + end
+		cmd = './chopchop.py -G ce11 -P -o ' + os.path.join(outputDir,r['name']) 
+		cmd+= ' ' + r.chrom + ':' + start + '-' + end + ' '
+		if extra_inputs >0:
+			for item in extra_inputs:
+				cmd+= item + ' '
 		print(cmd)
 		os.system(cmd)
 
 if __name__ == '__main__':
 	input_coords = sys.argv[1]
-	fastChopChop(input_coords)
+	extra_inputs = sys.argv[2:]
+	fastChopChop(input_coords, extra_inputs)
