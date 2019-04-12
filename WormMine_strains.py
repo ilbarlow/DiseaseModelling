79c3577206a6e693a2807891c9f42c790a0af24a
Ida <ilbarlow1@gmail.com>
2019-04-12 10:18:55 +0100

    StrainsUpdate

--- WormMine_strains.py
+++ WormMine_strains.py
@@ -12,23 +12,17 @@ import itertools
 
 service = Service("http://intermine.wormbase.org/tools/wormmine/service")
 
-if __name__ == '__main__':
-
+def find_strain(inputID, strainDF):
 	query = service.new_query('Gene')
-	query.add_view(
-		"strains.CGCReceived",\
-		"strains.genotype",\
-		"strains.laboratory",\
-		"strains.madeBy",\
-		"strains.mutagen",\
-		"strains.ncbiTaxonomyID",\
-		"strains.otherName",\
-		"strains.outcrossed",\
-		"strains.primaryIdentifier",\
-		"strains.species",\
-		"strains.remark",\
-		"strains.strainHistory"\
-		)
+	query.add_view("strains.*")
+	_q = query.where('Gene.primaryIdentifier', '=', inputID).results('dict')
+	print ('{} strains found for {}'.format(len(_q), inputID))
+	for row in _q:
+	    strainDF = strainDF.append(pd.Series(row), ignore_index=True)
+	return strainDF
+    
+
+if __name__ == '__main__':
 
 	inputIDs = sys.argv[1]
 	outputFile = sys.argv[2]
@@ -36,38 +30,47 @@ if __name__ == '__main__':
 	def findStrains(inputIDs, outputFile):
 
 		#open csv
-	    with io.open(inputIDs, 'r',  encoding='utf-8-sig') as fid:
-	    	 IDs = fid.read().split(',')
-	    	#remove dupliates
-	    IDs = np.unique(IDs)
-	    print (IDs)
+		with io.open(inputIDs, 'r',  encoding='utf-8-sig') as fid:
+			IDs = fid.read().split(',')
+			#remove dupliates
+		IDs = np.unique(IDs)
+		print (IDs)
+		CGCstrainsDF = pd.DataFrame()
+		for i in IDs:
+		    print ('Finding {}'.format(i))		    
+		    try:
+		        CGCstrainsDF = find_strain(i, CGCstrainsDF)
 
-        strainsDF = pd.DataFrame()
-        for i in IDs:
-            print ('Finding {}'.format(i))
-            total_strains = 0
-            try:
-                _q = query.where('Gene.primaryIdentifier', '=', i).results('dict')
-                print ('{} strains found for {}'.format(len(_q), i))
-                for row in _q:
-                    strainsDF = strainsDF.append(pd.Series(row), ignore_index=True)
-            except StopIteration:
-                print ('0 strains found for {}'.format(i))
-          
-	    # strainsDF[strainsDF['Gene.strains.genotype']!='']
-	    strainsDF = strainsDF.drop_duplicates(subset = 'Gene.strains.primaryIdentifier')
-	    strainsDF.to_csv(outputFile, index=False)
-        
-        strain_summary = {
-                'CGCstrains': list(set(np.unique(strainsDF['Gene.primaryIdentifier'])).intersection(IDs)),
-                'noCGCstrains': list(set(np.unique(strainsDF['Gene.primaryIdentifier'])).symmetric_difference(IDs))
-                }
-	    with open(os.path.join(os.path.dirname(inputIDs), 'strainSummary.csv'),'w') as f:
-            w = csv.writer(f)
-            w.writerow(strain_summary.keys())
-            w.writerows(itertools.zip_longest(*strain_summary.values()))
+		    except StopIteration:
+		        print ('strain error for {}'.format(i))
+		        # query = service.new_query('Gene')
+		        # query.add_view(
+		        # 		"strains.*"
+		        # 		)
+		        # CGCstrainsDF = find_strain(i, CGCstrainsDF)
+	        
+	        
+		# strainsDF[strainsDF['Gene.strains.genotype']!='']
+		CGCstrainsDF = CGCstrainsDF.drop_duplicates(subset = 'Gene.strains.primaryIdentifier')
+		CGCstrainsDF.to_csv(outputFile, index=False)
 
-        return
+		CGCstrainsDF['gene_summary'] = list(zip(CGCstrainsDF['Gene.primaryIdentifier'], CGCstrainsDF['Gene.symbol']))
 
-	findStrains(inputIDs, outputFile)
-	    
+		#make a dictionary of WBIDs and gene names
+		WBID_dict = {}
+		[WBID_dict.update({k:v}) for k,v in np.unique(CGCstrainsDF['gene_summary'])]
+
+		noCGC_strains = list(set(WBID_dict.keys()).symmetric_difference(IDs))
+		   
+		with open(os.path.join(os.path.dirname(inputIDs), 'CGCstrains.csv'),'w') as f:
+		    w = csv.writer(f)
+		    w.writerows(WBID_dict.items())
+		    
+		with open(os.path.join(os.path.dirname(inputIDs), 'noCGCstrains.csv'), 'w') as f:
+		    w = csv.writer(f)
+		    for i in noCGC_strains:
+		        w.writerow([i])
+
+		return
+
+findStrains(inputIDs, outputFile)
